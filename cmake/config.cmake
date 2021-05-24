9f4e23dbffde48b693bdce112bd7173594c5cfc4
Rasim Labibov <labibov.rasim@gmail.com>
2021-05-24 16:56:35 +0300

    Update CMake configs

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -5,18 +5,6 @@ macro(make_project_)
 
     project(${PROJECT} CXX)
 
-    if (MSVC)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W4")
-    else ()
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -Werror -std=c++11")
-    endif ()
-    
-    if (MSVC)
-        add_definitions(
-            -D_USE_MATH_DEFINES
-            )
-    endif ()
-
     if (NOT DEFINED HEADERS)
         file(GLOB HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/*.h)
     endif ()
@@ -29,9 +17,27 @@ macro(make_project_)
     source_group("Source Files" FILES ${SOURCES})
 endmacro ()
 
+macro(make_project_options_)
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+        target_compile_options(${PROJECT} PRIVATE /Wall)
+        target_compile_definitions(${PROJECT} PRIVATE _USE_MATH_DEFINES)
+    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        # Flags to compile HandmadeMath with Clang
+        target_compile_options(${PROJECT} PRIVATE
+            -Wall -Wextra -Werror -Wpedantic
+            -Wno-gnu-anonymous-struct -Wno-nested-anon-types -Wno-missing-field-initializers -Wno-missing-braces)
+    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+        # Flags to compile HandmadeMath with GCC
+        target_compile_options(${PROJECT} PRIVATE
+            -Wall -Wextra -Werror -Wpedantic
+            -Wno-missing-field-initializers -Wno-missing-braces)
+    endif ()
+endmacro()
+
 macro(make_executable)
     make_project_()
     add_executable(${PROJECT} ${HEADERS} ${SOURCES})
+    make_project_options_()
 
     set(CMAKE_INSTALL_PREFIX "${CMAKE_SOURCE_DIR}/bundle/${PROJECT}")
     install(
@@ -45,6 +51,7 @@ endmacro()
 macro(make_library)
     make_project_()
     add_library(${PROJECT} STATIC ${HEADERS} ${SOURCES})
+    make_project_options_()
     target_include_directories(${PROJECT} INTERFACE ${CMAKE_CURRENT_SOURCE_DIR})
 
     set_target_properties(${PROJECT} PROPERTIES FOLDER Libraries)
